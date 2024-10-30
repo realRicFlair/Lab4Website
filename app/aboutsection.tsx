93424f827721436ee311315db7c60a607dba1f84
Adithya Ambagaspitiya <144413861+realRicFlair@users.noreply.github.com>
2024-10-29 22:37:26 -0700

    Update aboutsection.tsx

--- app/aboutsection.tsx
+++ app/aboutsection.tsx
@@ -7,7 +7,7 @@ export default function AboutSection(){
             <HeadingDivider title="About Me"/>
             <div className="pt-10 pb-16 max-w-5xl flex flex-col gap-3">
                 <p className="text-lg"> I also like 40k </p>
-                <img src="SM2.jpg" width="250" height="250" alt="Space Marine 2 Cover Art" />
+                <img src="/public/SM2.jpg" width="250" height="250" alt="Space Marine 2 Cover Art" />
 
                 <p>
                     The new game was pretty cool<br />
